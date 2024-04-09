15e420a9c642b5c04091ca777af4d2b24ff05041
ShutongB <baoshutong@gmail.com>
2024-04-09 13:06:17 -0600

    update

---
+++ README.md
@@ -0,0 +1,20 @@
+# AWS Cloud Resume Challenge
+
+
+I've embarked on the Cloud Resume Challenge using AWS. 
+What exactly is the AWS Cloud Resume Challenge? - [The Cloud Resume Challenge](https://cloudresumechallenge.dev/docs/the-challenge/aws/) t's an intricate resume-building project, designed to cultivate and showcase essential skills for a cloud computing career. The initiative was introduced by Shutong Bao.
+## Architecture
+
+![Architecture Diagram](/architecture.png)
+
+**Services Used**:
+
+- S3
+- AWS CloudFront
+- Certificate Manager
+- AWS Lambda
+- Dynamo DB
+- GitHub Actions
+- Terraform
+
+  
---
+++ website/text.py
@@ -0,0 +1,17 @@
+        #       <!-- work -->
+        #       <section class="work py-5 px-4 bg-light" id="portfolio">
+        #         <div class="py-3">
+        #             <h5 class="text-uppercase font-os font-size-16 text-muted">Experience</h5>
+        #             <h3 class="mb-0">Online Teaching Assistant</h3>
+        #             <div class="subheading mb-3">CIT 325 Database Programming | BYU-Idaho</div>
+
+        #             <p>•	Assisted in teaching and guiding over 50 students through complex database concepts and PL/SQL programming in an Oracle environment. 
+        #               </p>
+        #               <p>•	Supported students in lab assignments and projects, enhancing their understanding of SQL querying and database management best practices.</p>
+        #               <p>•	Helped students in conceptualizing, designing, and implementing database applications, ensuring a practical understanding of course materials.</p>
+        #               <p>•	Provided troubleshooting support for students, specifically in setting up and managing AWS accounts and accessing Ubuntu servers, ensuring seamless technical integration for course assignments.</p>
+        #         </div>
+        #         <div class="resume-date text-md-right">
+        #            <span class="text-primary">January 2024 - Present</span>
+        #         </div>
+        #   </div>
